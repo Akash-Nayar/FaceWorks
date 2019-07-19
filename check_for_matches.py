b8d5c3be06eb8b01f1b92f6f5b31bd1256aa0968
apumaparna <aparnakumargs@gmail.com>
2019-07-19 10:40:35 -0400

    edited the function so that entering the database name is unnecessary

--- check_for_matches.py
+++ check_for_matches.py
@@ -1,18 +1,23 @@
-def check_for_match(vectorToCheck, database):
+def check_for_match(vectorToCheck):
     """
     Checks the vector against the mean descriptor vectors in the database. If the the two
     vectors are similar, then it is concluded to be a match. Matched descriptor vectors are
     added for people. If the vector is unknown, then a new person is added to the database.
     :param vectorToCheck: numpy.ndarray of size (128,)
-    :param database: dict of the name(key), and the mean descriptors, descriptors, and number of descriptors (values)
     :return: match: boolean, True if there is a match, False if there is not a match
     """
     import numpy as np
     import mean_face as mf
     import addto_database as addb
+    import pickle
 
     #Set up a check value
     check = 20
+    match = False
+
+    #Unpickling a database
+    with open("faceworks.pkl", mode="rb") as opened_file:
+        database = pickle.load(opened_file)
 
     #First identifies the mean vector in the database
     for key, value in database:
@@ -29,21 +34,26 @@ def check_for_match(vectorToCheck, database):
         match = True if (diff < check) else match=False
         # If match is true, the descriptor vector is then added to the database for the match.
         if match:
+            #Setting up values
             vectors = value[1]
             vectorCount = value[2]
 
+            #The descriptor vector is appended to the list of vectors, and the number of vectors is updated
+            #A new mean vector is calculated and set.
             vectors.append(vectorToCheck)
             vectorCount += 1
-
             meanVector = mf.mean_face(key)
+
+            #The database is then updated accordingly
+            database.update({key: value})
 
             break
 
     # If the match is false, a new person is created in the database.
-    else:
-        addb.addTo_database(vectorToCheck)
+    if not match:
+        addb.add_to_database(vectorToCheck)
 
-    #Returns match.
+    #Returns match
     return match
 
 
