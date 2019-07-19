1c812df4ba3780cc5ebf64f554e603724f819755
ValianceX <sydney.karimi02@gmail.com>
2019-07-19 14:37:36 -0400

    fixed mean_face error

--- mean_face.py
+++ mean_face.py
@@ -9,13 +9,11 @@ def mean_face(name):
         mean descriptor vector for the person in the database
     """
     #load the database into the file
-    pickle_in = open("faces.pickle", "rb")
+    pickle_in = open("faceworks.pickle", "rb")
     database = pickle.load(pickle_in)
 
     #get the mean of the vectorsS
-    d_vectors = database.values()
-    d_vectors = np.array( [d_vectors[i][1] for i in range(len(d_vectors))] )
-
+    d_vectors = database[name][0]
     d_mean = np.mean(d_vectors, axis=0)
 
     return d_mean
